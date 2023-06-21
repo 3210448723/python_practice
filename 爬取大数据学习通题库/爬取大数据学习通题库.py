69e676324414690a07908add8bacd672a5d0f4eb
3210448723 <61307277+3210448723@users.noreply.github.com>
2023-06-21 15:12:04 +0800

    Update 爬取大数据学习通题库.py

--- 爬取大数据学习通题库/爬取大数据学习通题库.py
+++ 爬取大数据学习通题库/爬取大数据学习通题库.py
@@ -33,7 +33,7 @@ def writeline(content):
 
 def get_chapter_problem(url):
 	cookies = {
-		'Cookie': 'web_im_hxToken=YWMta4SSEuiTEe2I7NcLnmSvO6KlE4D%2d6RHksYgJxSsb8jJ7KgCwAjMR64wG1WtFLx0zAwMAAAGH2lnoVDht7EASR7qAYD4MYG%5f3E1uHeVxp4Ynx%5faXHLNQMPaIpixw2Ww; web_im_tid=106970573; web_im_pic=http%3a%2f%2fphoto%2echaoxing%2ecom%2fphoto%5f80%2ejpg; web_im_name=%u8881%u91d1%u654f; KI4SO_SERVER_EC=RERFSWdRQWdsckNwckxwM2pXeFFSZWxWaTdSOTdVYXlRaGg3SElDUXRGWFNnTDU0SlZkcjh6VVM2%0AaXhPNnFUMERtU2djWlRjU2NTVQp1NG13bWtscWxZTWZ4Vk9iTWtPQVBwMVlhUkdjQWk0N3dlclJt%0ARDFtdWE0OVd4bXBHSTZockJmNDRYdlVvNVZVdmpjVGZ2STd3VEh1Ck5EcWZVcTVkd0srUnpqZy9H%0AKzZmNVozWE83dW9rdUdhMzl1cDR3V0NMYUpXOFk5bERvRG9ZODA4d002UEFoQkw1NWZMR1hYd3Rm%0AL3EKRXNpYXFmR0wrQzFVYXZZaytuamcwOGNodEJGR3IzZytOak1OQjlWRWxidWRBcXhLMHVkVkxF%0AUzNib2JiNDhLeU1RM0QweDNIc0hYSgpsYjVjbUhFeDFydEJQQUlWclZTUHJRUzh6dk1DVHBib3FS%0AU2g3YVVOYkNFbWFKRUZzZHkrWXVCUjQwNUZpSlRSSEhucE5nSk9sdWlwCkZLSHRVNWlsM2ExQlR5%0AaWRFVnk5dkxDYXorTCtWWUtXVG5DMU1lNDBPcDlTcmwyckxocXM2WDluVVNueXNUbjFXdEhKP2Fw%0AcElkPTEma2V5SWQ9MQ%3D%3D; _tid=106970573; sso_puid=146169861; _industry=5; source=""; lv=2; fid=1821; _uid=146169861; uf=b2d2c93beefa90dcd1ee03ad65e10855c0ebe4a8b6884f65494558de4c24a9e332b58050637915d954c0082f4f1fc7ff913b662843f1f4ad6d92e371d7fdf6446ece1d9f47db742bce71fc6e59483dd32d3afb659c1c0b6869176f8518a79bc6366a439d4eba99ad; _d=1687250472281; UID=146169861; vc=250310B88D674D8CE6EB64CCB2B1C832; vc2=83914FBF11EC1639503B02ABA9DAA265; vc3=KCv7RDhfH%2Fn%2BmXL9LDOJ26215Hiul1%2BN4QYcjaL8hYjvrmCVROI3kizCezaxmQdwX%2FzH%2FBDdtr4%2B1idyRS5xVTp1EG9W4w7CNRxKfYP1FA4oapyeyyDNAkbtg9k3FNKT5Ss8rl82ZPf3leboOEABRyROSTbh1de%2B%2BkFgKIQM1Qs%3Db93671deba42cb7b27825b0d07caf436; cx_p_token=fe06b51690a2ec1453025c13023a08b4; xxtenc=2a6dad7ef1fd199294a22900361bc35c; DSSTASH_LOG=C_38-UN_55-US_146169861-T_1687250472283; thirdRegist=0; tl=1; k8s=1687250483.898.54025.98161; jrose=1DEB2C1F015852EA0CD3CBC52B6EB991.mooc-p3-1748421541-w1cn6; route=0eb899bb9bb390391b050e8cb1d78cb4'
+		'Cookie': '对应链接的cookie，自己F12查看'
 	}
 	# 获取某个章节的题目和答案
 	resp = requests.post(url=url, cookies=cookies, headers=headers)
@@ -237,21 +237,7 @@ def get_chapter_problem(url):
 
 if __name__ == '__main__':
 	cookies = {
-		'Cookie': 'web_im_hxToken=YWMta4SSEuiTEe2I7NcLnmSvO6KlE4D'
-		          '%2d6RHksYgJxSsb8jJ7KgCwAjMR64wG1WtFLx0zAwMAAAGH2lnoVDht7EASR7qAYD4MYG%5f3E1uHeVxp4Ynx'
-		          '%5faXHLNQMPaIpixw2Ww; web_im_tid=106970573; '
-		          'web_im_pic=http%3a%2f%2fphoto%2echaoxing%2ecom%2fphoto%5f80%2ejpg; web_im_name=%u8881%u91d1%u654f; '
-		          'KI4SO_SERVER_EC=RERFSWdRQWdsckNwckxwM2pXeFFSZWxWaTdSOTdVYXlRaGg3SElDUXRGWFNnTDU0SlZkcjh6VVM2'
-		          '%0AaXhPNnFUMERtU2djWlRjU2NTVQp1NG13bWtscWxZTWZ4Vk9iTWtPQVBwMVlhUkdjQWk0N3dlclJt'
-		          '%0ARDFtdWE0OVd4bXBHSTZockJmNDRYdlVvNVZVdmpjVGZ2STd3VEh1Ck5EcWZVcTVkd0srUnpqZy9H'
-		          '%0AKzZmNVozWE83dW9rdUdhMzl1cDR3V0NMYUpXOFk5bERvRG9ZODA4d002UEFoQkw1NWZMR1hYd3Rm'
-		          '%0AL3EKRXNpYXFmR0wrQzFVYXZZaytuamcwOGNodEJGR3IzZytOak1OQjlWRWxidWRBcXhLMHVkVkxF'
-		          '%0AUzNib2JiNDhLeU1RM0QweDNIc0hYSgpsYjVjbUhFeDFydEJQQUlWclZTUHJRUzh6dk1DVHBib3FS'
-		          '%0AU2g3YVVOYkNFbWFKRUZzZHkrWXVCUjQwNUZpSlRSSEhucE5nSk9sdWlwCkZLSHRVNWlsM2ExQlR5'
-		          '%0AaWRFVnk5dkxDYXorTCtWWUtXVG5DMU1lNDBPcDlTcmwyckxocXM2WDluVVNueXNUbjFXdEhKP2Fw'
-		          '%0AcElkPTEma2V5SWQ9MQ%3D%3D; _tid=106970573; sso_puid=146169861; _industry=5; lv=2; fid=1821; '
-		          '_uid=146169861; '
-		          'uf=b2d2c93beefa90dcd1ee03ad65e10855c0ebe4a8b6884f65494558de4c24a9e332b58050637915d954c0082f4f1fc7ff913b662843f1f4ad6d92e371d7fdf6446ece1d9f47db742bce71fc6e59483dd32d3afb659c1c0b6869176f8518a79bc6366a439d4eba99ad; _d=1687250472281; UID=146169861; vc=250310B88D674D8CE6EB64CCB2B1C832; vc2=83914FBF11EC1639503B02ABA9DAA265; vc3=KCv7RDhfH%2Fn%2BmXL9LDOJ26215Hiul1%2BN4QYcjaL8hYjvrmCVROI3kizCezaxmQdwX%2FzH%2FBDdtr4%2B1idyRS5xVTp1EG9W4w7CNRxKfYP1FA4oapyeyyDNAkbtg9k3FNKT5Ss8rl82ZPf3leboOEABRyROSTbh1de%2B%2BkFgKIQM1Qs%3Db93671deba42cb7b27825b0d07caf436; cx_p_token=fe06b51690a2ec1453025c13023a08b4; xxtenc=2a6dad7ef1fd199294a22900361bc35c; DSSTASH_LOG=C_38-UN_55-US_146169861-T_1687250472283; tl=1; thirdRegist=0; k8s=1687256149.96.55410.800473; route=1ab934bb3bbdaaef56ce3b0da45c52ed; jrose=1855551286E25CEF78B10985CCF808E4.mooc-p3-3523333032-2rcbh'}
+		'Cookie': '对应链接的cookie，自己F12查看'}
 	url = 'https://mooc1.chaoxing.com/mooc2/work/list?courseId=234766790&classId=77297858&cpi=156529108&ut=s&enc=27f8dfcdcda0b97e1d928bfc6e13fa57'
 	resp = requests.get(url=url, cookies=cookies, headers=headers)
 	resp = resp.text
