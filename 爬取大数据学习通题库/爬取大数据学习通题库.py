aadbfa8897e0ac6b7f8f21f03547b2bc3f02e8a6
3210448723 <61307277+3210448723@users.noreply.github.com>
2023-06-22 15:34:41 +0800

    取消删除题干文字的空格

--- 爬取大数据学习通题库/爬取大数据学习通题库.py
+++ 爬取大数据学习通题库/爬取大数据学习通题库.py
@@ -14,21 +14,23 @@ headers = {
 	              'Chrome/86.0.4240.198 Safari/537.36',
 }
 # 数据结构：
+# 本来想之后统一存储的，但是还是选择爬一道题存一道题，因此一直没用到
 # problem_list[ problem{pid : p_content[type, title, select[ {sid : s_content}, {sid : s_content}, ...], answer]}]
 problem_list = []
 problem = {}
 p_content = []
 select = []
 cookies = {
-	'Cookie': '对应链接的cookie，自己F12查看'}
+	'Cookie': '对应链接的cookie，自己F12查看'
+}
 
 
-def initwrite():
+def init_write():
 	with open('大数据学习通题库.md', 'w', encoding='utf-8') as file:
 		pass
 
 
-def writeline(content):
+def write_line(content):
 	with open('大数据学习通题库.md', 'a', encoding='utf-8') as file:
 		file.write(content + '\n')
 
@@ -52,7 +54,7 @@ def get_chapter_problem(url):
 		print(type)
 		if type.find('单选题') != -1:
 			type = '单选题'
-			writeline('## ' + type + '\n')
+			write_line('## ' + type + '\n')
 			# continue
 			# 遍历单选题列表
 			for problem_one_select in i.xpath('./div'):
@@ -62,15 +64,15 @@ def get_chapter_problem(url):
 				select = []
 
 				# 获取题目编号pid和题干title
-				temp = problem_one_select.xpath('string(./h3)').replace(" ", "").split('.')
+				temp = problem_one_select.xpath('string(./h3)').split('.')
 				pid = temp[0]
 				title = temp[1]
 				print('第', pid, '题', title)
-				writeline(pid + '. ' + title + '\n')
+				write_line(pid + '. ' + title + '\n')
 				select_lis = problem_one_select.xpath('./ul[1]/li')
 				# 遍历选项
 				for li in select_lis:
-					temp = li.xpath('string()').replace(" ", "").split('.')
+					temp = li.xpath('string()').split('.')
 					# 选项sid：A B C D
 					sid = temp[0]
 					# print(sid)
@@ -78,14 +80,14 @@ def get_chapter_problem(url):
 					# 选项对应的语句
 					s_content = temp[1]
 					print('选项', sid, '：', s_content)
-					writeline(sid + ' ' + s_content + '\n')
+					write_line(sid + ' ' + s_content + '\n')
 
 					select.append({sid: s_content})
 				# 获取答案
 				answer = problem_one_select.xpath('./div[@class="mark_answer"]/div[@class="mark_key clearfix"]/span['
-				                                  '@class="colorGreen marginRight40 fl"]/text()')[0].replace(" ", "")
+				                                  '@class="colorGreen marginRight40 fl"]/text()')[0]
 				print('正确答案：', answer)
-				writeline('\n正确答案：\n' + answer + '\n')
+				write_line('\n正确答案：\n' + answer + '\n')
 
 				# 装入数据结构
 				p_content.append(type)
@@ -100,7 +102,7 @@ def get_chapter_problem(url):
 		elif type.find('多选题') != -1:
 			# 逻辑和单选题一模一样
 			type = '多选题'
-			writeline('## ' + type + '\n')
+			write_line('## ' + type + '\n')
 
 			# continue
 			# 遍历多选题列表
@@ -111,16 +113,16 @@ def get_chapter_problem(url):
 				select = []
 
 				# 获取题目编号pid和题干title
-				temp = problem_multi_select.xpath('string(./h3)').replace(" ", "").split('.')
+				temp = problem_multi_select.xpath('string(./h3)').split('.')
 				pid = temp[0]
 				title = ''.join(temp[1:])
 				print('第', pid, '题', title)
-				writeline(pid + '. ' + title + '\n')
+				write_line(pid + '. ' + title + '\n')
 
 				select_lis = problem_multi_select.xpath('./ul[1]/li')
 				# 遍历选项
 				for li in select_lis:
-					temp = li.xpath('string()').replace(" ", "").split('.')
+					temp = li.xpath('string()').split('.')
 					# 选项sid：A B C D
 					sid = temp[0]
 					# print(sid)
@@ -128,14 +130,14 @@ def get_chapter_problem(url):
 					# 选项对应的语句
 					s_content = ''.join(temp[1:])
 					print('选项', sid, '：', s_content)
-					writeline(sid + ' ' + s_content + '\n')
+					write_line(sid + ' ' + s_content + '\n')
 
 					select.append({sid: s_content})
 				# 获取答案
 				answer = problem_multi_select.xpath('./div[@class="mark_answer"]/div[@class="mark_key clearfix"]/span['
-				                                    '@class="colorGreen marginRight40 fl"]/text()')[0].replace(" ", "")
+				                                    '@class="colorGreen marginRight40 fl"]/text()')[0]
 				print('正确答案：', answer)
-				writeline('\n正确答案：\n' + answer + '\n')
+				write_line('\n正确答案：\n' + answer + '\n')
 
 				# 装入数据结构
 				p_content.append(type)
@@ -149,7 +151,7 @@ def get_chapter_problem(url):
 		# print(problem_list)
 		elif type.find('简答题') != -1:
 			type = '简答题'
-			writeline('## ' + type + '\n')
+			write_line('## ' + type + '\n')
 
 			# continue
 			# 遍历简答题列表
@@ -161,14 +163,14 @@ def get_chapter_problem(url):
 				select = []
 
 				# 获取题目编号pid和题干title
-				temp = problem_brief.xpath('string(./h3[1])').replace(" ", "").split('.')  # 有的地方的简答题题干爬取不到，如第10章的12和15题
+				temp = problem_brief.xpath('string(./h3[1])').split('.')  # 有的地方的简答题题干爬取不到，如第10章的12和15题
 				title = problem_brief.xpath('string(./p)')  # 虽然不知道为什么但是这样可以跑...
 				pid = temp[0]
 				score = temp[1].split(')')[0] + ')'
 				# 应对题干爬取失效的情况
 				title = temp[1].split(')')[1] == '' and title or temp[1].split(')')[1]
 				print('第', pid, '题', score, title)
-				writeline(pid + '. ' + score + title + '\n')
+				write_line(pid + '. ' + score + title + '\n')
 
 				# 获取答案，html标签也带上，因为有的答案是表格形式
 				answer = problem_brief.xpath('./div[@class="mark_answer"]/div[@class="mark_answer_key pad0"]/dl['
@@ -185,7 +187,7 @@ def get_chapter_problem(url):
 					'</dd>', '').replace('\t', '').replace('\n\n', '').replace('<br/>', '').replace('<br>', '').strip()
 				# 太多了，不输出
 				# print('正确答案：', answer)
-				writeline('\n正确答案：\n' + answer + '\n')
+				write_line('\n正确答案：\n' + answer + '\n')
 
 				# 装入数据结构
 				p_content.append(type)
@@ -199,7 +201,7 @@ def get_chapter_problem(url):
 		# print(problem_list)
 		elif type.find('填空题') != -1:
 			type = '填空题'
-			writeline('## ' + type + '\n')
+			write_line('## ' + type + '\n')
 
 			# continue
 			# 遍历填空题列表
@@ -211,18 +213,18 @@ def get_chapter_problem(url):
 				select = []
 
 				# 获取题目编号pid和题干title
-				temp = problem_blank.xpath('string(./h3[1])').replace(" ", "").split('.')
+				temp = problem_blank.xpath('string(./h3[1])').split('.')
 				pid = temp[0]
 				title = ''.join(temp[1:])
 				print('第', pid, '题', title)
-				writeline(pid + '. ' + title + '\n')
+				write_line(pid + '. ' + title + '\n')
 
 				# 获取答案，html标签也带上，因为有的答案是表格形式
 				answer = problem_blank.xpath('./div[@class="mark_answer"]/div[1]/dl['
 				                             '@class="mark_fill colorGreen"]/dd/text()')
 				answer = '\n'.join(answer)
 				print('正确答案：', answer)
-				writeline('\n正确答案：\n' + answer + '\n')
+				write_line('\n正确答案：\n' + answer + '\n')
 
 				# 装入数据结构
 				p_content.append(type)
@@ -239,7 +241,8 @@ def get_chapter_problem(url):
 
 
 if __name__ == '__main__':
-	url = 'https://mooc1.chaoxing.com/mooc2/work/list?courseId=234766790&classId=77297858&cpi=156529108&ut=s&enc=27f8dfcdcda0b97e1d928bfc6e13fa57'
+	url = 'https://mooc1.chaoxing.com/mooc2/work/list?courseId=234766790&classId=77297858&cpi=156529108&ut=s&enc' \
+	      '=27f8dfcdcda0b97e1d928bfc6e13fa57'
 	resp = requests.get(url=url, cookies=cookies, headers=headers)
 	resp = resp.text
 	# print(resp)
@@ -249,14 +252,14 @@ if __name__ == '__main__':
 	chapter_xpath = '/html/body/div[2]/div/div/div[2]/div[2]/ul/li'
 	chapters = etree_html.xpath(chapter_xpath)
 
-	initwrite()
+	init_write()
 
 	for chapter in chapters:
 		chapter_url = chapter.xpath('./@data')[0]
 		chapter_name = chapter.xpath('./@aria-label')[0]
 		print(chapter_name, chapter_url)
-		writeline('# [' + chapter_name + '](' + chapter_url + ')\n')
+		write_line('# [' + chapter_name + '](' + chapter_url + ')\n')
 		get_chapter_problem(chapter_url)
-		# break
+	# break
 
 # 将题库写入md中
